87c159bd6417e55022c6eb5d829416f2feaa5ba3
mh.kim <mh.kim@dnd-lab.com>
2022-07-12 10:41:06 +0900

    시뮬레이터 상단 플로팅 버튼 추가

---
+++ src/components/FloatButton.vue
@@ -0,0 +1,67 @@
+<template>
+  <wrapper v-bind="props">
+    <img :src="icon" alt="">
+    <p>{{ text }}</p>
+  </wrapper>
+</template>
+
+<script>
+import { vw } from '@/styles/mixin';
+import styled from 'vue3-styled-components';
+
+const baseProps = {
+  icon: {
+    type: String,
+    default: '',
+  },
+  iconWidth: {
+    type: Number,
+    default: 32,
+  },
+  text: {
+    type: String,
+    default: '',
+  },
+};
+
+const Wrapper = styled('div', baseProps)`
+  position: relative;
+  ${vw('width', 90)}
+  ${vw('height', 70)}
+  ${vw('border-radius', 70)}
+  background-color: rgba(0, 0, 0, 0.8);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  color: ${({ theme }) => theme.colors.white};
+  ${vw('font-size', 20)}
+  font-weight: bold;
+  > img {
+    ${vw('width', 35)}
+  }
+  ${({ text, iconWidth }) => text.length && `
+    ${vw('width', 182)}
+    > p {
+      ${vw('padding-left', 50)}
+    }
+    > img {
+      position: absolute;
+      ${vw('width', iconWidth)}
+      ${vw('left', 20)}
+      top: 50%;
+      transform: translateY(-50%);
+    }
+  `}
+`;
+
+export default {
+  components: { Wrapper },
+  props: baseProps,
+  setup(props) {
+    return {
+      props,
+    };
+  },
+};
+</script>
--- src/components/SimulatorContainer.vue
+++ src/components/SimulatorContainer.vue
@@ -1,15 +1,69 @@
 <template>
-  <wrapper></wrapper>
+  <wrapper>
+    <div>
+      <img v-if="getInterior" :src="getInterior.image.default" alt="">
+    </div>
+    <div>
+      <float-button
+        icon="/images/floating-icon-home.png"
+      />
+      <float-button
+        icon="/images/floating-icon-interior-crop.png"
+        :iconWidth="32"
+        text="INTERIOR"
+      />
+      <float-button
+        icon="/images/floating-icon-refrigerator.png"
+        :iconWidth="26"
+        text="PRODUCT"
+      />
+      <float-button
+        icon="/images/product-detail.png"
+      />
+    </div>
+  </wrapper>
 </template>
 
 <script>
 import styled from 'vue3-styled-components';
+import { useGetters } from '@/store/helper';
+import { vw } from '@/styles/mixin';
+import FloatButton from './FloatButton.vue';
 
 const Wrapper = styled.div`
-
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  left: 0;
+  top: 0;
+  > div {
+    position: absolute;
+    :nth-child(1) {
+      left: 0;
+      top: 0;
+      img {
+        width: 100vw;
+        height: 100vh;
+      }
+    }
+    :nth-child(2) {
+      ${vw('left', 40)}
+      ${vw('top', 28)}
+      display: grid;
+      grid-template-columns: 1fr 2fr 2fr 1fr;
+      ${vw('grid-column-gap', 20)}
+    }
+  }
 `;
 
 export default {
-  components: { Wrapper },
+  components: { Wrapper, FloatButton },
+  setup() {
+    const { getInterior } = useGetters();
+
+    return {
+      getInterior,
+    };
+  },
 };
 </script>
