ced2f5d6066d30ad64287dabd5fcc48d32cd3273
mh.kim <mh.kim@dnd-lab.com>
2022-07-13 16:16:38 +0900

    mixin media query 추가

--- src/styles/mixin.js
+++ src/styles/mixin.js
@@ -1,21 +1,26 @@
 const screenSize = 1440;
 
-const calc = (value) => {
-  if (window.innerWidth > screenSize) {
-    return `${value}px`;
-  }
-
-  return `${(value * (100 / screenSize) * 1).toFixed(4)}vw`;
-};
+const calc = (value) => `${(value * (100 / screenSize) * 1).toFixed(4)}vw`;
 const vw = (attribute, value) => {
   if (Number.isInteger(value)) {
-    return `${attribute}: ${calc(value)};`;
+    return `
+      ${attribute}: ${value}px;
+      @media (max-width: ${screenSize}px) {
+        ${attribute}: ${calc(value)};
+      }
+    `;
   }
 
   if (Array.isArray(value)) {
-    const itemList = value.map((item) => (Number.isInteger(item) ? calc(item) : item));
+    const itemList = value.map((item) => (Number.isInteger(item) ? `${item}px` : item));
+    const tabletItemList = value.map((item) => (Number.isInteger(item) ? calc(item) : item));
 
-    return `${attribute}: ${itemList.join(' ')};`;
+    return `
+      ${attribute}: ${itemList.join(' ')};
+      @media (max-width: ${screenSize}px) {
+        ${attribute}: ${tabletItemList.join(' ')};
+      }
+    `;
   }
 
   return '';
