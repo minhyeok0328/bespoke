c2c37b11b7c76186a0feb90782922ee798f3861e
mh.kim <mh.kim@dnd-lab.com>
2022-07-14 09:09:08 +0900

    interior 스탭 추가

---
+++ src/views/InteriorPage.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <b-step-header
+      title="INTERIOR Choose your type"
+      next-text="Product"
+      :step="['01', '02']"
+      :next-callback="moveProductPage"
+    />
+  </div>
+</template>
+
+<script>
+import BStepHeader from '@/components/BStepHeader.vue';
+import { useRouter } from 'vue-router';
+
+export default {
+  components: { BStepHeader },
+  setup() {
+    const router = useRouter();
+    const moveProductPage = () => {
+      router.push({ name: 'ProductPage' });
+    };
+
+    return {
+      moveProductPage,
+    };
+  },
+};
+</script>
